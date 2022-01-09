e35dae03240c569d60d100bc54c854d826b3f4b4
tungnx7 <ngotung7795@gmail.com>
2022-01-09 12:55:45 +0700

    first demo react

---
+++ demo-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Accordion from './components/Accordion'
+
+function App() {
+  return (
+    <div className="App">
+      <Accordion heading="Heading">
+        This is a content
+      </Accordion>
+        
+    </div>
+  );
+}
+
+export default App;
